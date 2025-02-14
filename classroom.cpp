d36aa08021cdc6a90aee5f442a4e2b3e07c4a963
Saleh Karami <msalehkaramibekr@gmail.com>
2025-02-13 18:34:20 -0800

    Wrote instructions on how to move forward.

--- classroom.cpp
+++ classroom.cpp
@@ -4,3 +4,5 @@ int main(){
   std::cout << "Haiiii Saleh !!!!!!!!" << std::endl;
   return 0;
 }
+
+// Plan is to set this up in such a manner where the program says "Your wish is my command.." then proceeds to perform whichever action I require of it.
